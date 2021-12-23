8aee4e75f6351d02d0d4e75456bb044bc78d2549
Celiina Valdner <86053881+Celtzu@users.noreply.github.com>
2021-12-23 21:26:40 +0200

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,30 @@
+function checkTemp(a){
+
+    if (a<21){
+
+        console.log("liiga külm")
+
+    return -1
+
+}
+
+    else if (a>40){
+
+        console.log("liiga kuum")
+
+    return 0
+
+    }
+
+    else{
+
+        console.log("paras")
+
+    return 0
+
+    }
+
+}
+
+
+checkTemp(30)
